bafb13a6b7792c3e8f18a6e818eaf8f6f1d3b10e
Hiroshi Yoshida <hrysd22@gmail.com>
2017-02-27 02:29:49 +0900

    WIP

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,57 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+//
+
+Vue.component('remarks', {
+  template: `
+    <section :class='kind'>
+      <header>
+        <h2>{{kind}} TODO: Capitalize</h2>
+      </header>
+
+      <ul>
+        <remark
+          v-for='(remark, index) in remarks'
+          :remark='remark'
+        >
+        </remark>
+      </ul>
+
+      <remark-form />
+    </section>
+  `,
+  props: ['kind', 'remarks']
+});
+
+Vue.component('remark', {
+  template: `
+    <li>
+      <p>{{remark.content}}</p>
+
+      <span class='thumbs-up'>
+        <i class='fa fa-thumbs-o-up'></i>
+        {{remark.reactions_count}}
+      </span>
+
+      <span class='remove-button'>
+        <i class='fa fa-times'></i>
+      </span>
+    </li>
+  `,
+  props: ['remark']
+});
+
+Vue.component('remark-form', {
+  template: `
+    <form class='inline-form'>
+      <div class='form-item'>
+        <input type='text'>
+      </div>
+
+      <div class='form-button'>
+        <button>Add</button>
+      </div>
+    </form>
+  `,
+});
