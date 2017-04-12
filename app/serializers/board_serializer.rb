e72896d287bbb1e54f97298089fcfba0d510e305
Hiroshi Yoshida <hrysd22@gmail.com>
2017-04-12 09:32:27 +0900

    Specify sort order

--- app/serializers/board_serializer.rb
+++ app/serializers/board_serializer.rb
@@ -1,14 +1,14 @@
 class BoardSerializer < ActiveModel::Serializer
   has_many :keeps do
-    object.remarks.keep
+    object.remarks.keep.created_at_asc
   end
 
   has_many :problems do
-    object.remarks.problem
+    object.remarks.problem.created_at_asc
   end
 
   has_many :tris do
-    object.remarks.tri
+    object.remarks.tri.created_at_asc
   end
 
   attributes :id, :title, :permalink, :state
