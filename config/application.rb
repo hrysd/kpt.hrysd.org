9077db8e38255cb5699e3df47e1ce1df00162424
Hiroshi Yoshida <hrysd22@gmail.com>
2020-06-17 02:08:51 +0900

    Execute `rails app:update`

--- config/application.rb
+++ config/application.rb
@@ -1,16 +1,6 @@
 require_relative 'boot'
 
-require "rails"
-# Pick the frameworks you want:
-require "active_model/railtie"
-require "active_job/railtie"
-require "active_record/railtie"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "action_view/railtie"
-require "action_cable/engine"
-require "sprockets/railtie"
-# require "rails/test_unit/railtie"
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -18,16 +8,12 @@ Bundler.require(*Rails.groups)
 
 module Kpt
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-    config.x.lets_encrypt_request_key  = ENV['LETS_ENCRYPT_REQUEST_KEY']
-    config.x.lets_encrypt_response_key = ENV['LETS_ENCRYPT_RESPONSE_KEY']
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.0
 
-    config.generators do |g|
-      g.stylesheets false
-      g.javascripts false
-      g.helper false
-    end
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
   end
 end
